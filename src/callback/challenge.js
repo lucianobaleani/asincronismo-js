7ee8ad6e60c81302b32e78a596dcee4b6c6f4588
Luciano <lucianobaleani94@gmail.com>
2025-09-07 12:32:28 -0300

    [ADD][fetchData]

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,4 +1,4 @@
-const XMLHttpRequest = require('xmlhttprequest');
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 const API = 'https://api.escuelajs.com/api/v1';
 
 function fetchData(urlAPI, callback){
@@ -17,3 +17,17 @@ function fetchData(urlAPI, callback){
     }
     xhttp.send();
 }
+
+
+fetchData(`${API}/products`, function(error1, data1){
+    if (error1) return console.error(error1);
+    fetchData(`${API}/products/${data1[0].id}`, function(error2, data2){
+        if (error2) return console.error(error2);
+        fetchData(`${API}/categories/${data2?.category?.id}`, function(error3, data3){
+            if (error3) return console.error(error3);
+            console.log(data1[0]);
+            console.log(data2.title);
+            console.log(data3.name);
+        })
+    })
+})
